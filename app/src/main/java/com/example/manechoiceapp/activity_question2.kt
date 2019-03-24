96d714e982233818e117c44ff65cd65467abd10b
rsamps1 <32546267+rsamps1@users.noreply.github.com>
2019-03-24 12:06:39 -0400

    First commit RS

---
+++ app/src/main/java/com/example/manechoiceapp/activity_question2.kt
@@ -0,0 +1,12 @@
+package com.example.manechoiceapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_question2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question2)
+    }
+}
