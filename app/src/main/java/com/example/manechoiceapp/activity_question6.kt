489de59e5b5f80c1f525913fd9f9d8add47e4519
rsamps1 <32546267+rsamps1@users.noreply.github.com>
2019-03-24 13:07:44 -0400

    changed file names to question 5 and 6

--- app/src/main/java/com/example/manechoiceapp/activity_question6.kt
+++ app/src/main/java/com/example/manechoiceapp/activity_question6.kt
@@ -7,6 +7,6 @@ class activity_question2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_question2)
+        setContentView(R.layout.activity_question6)
     }
 }
